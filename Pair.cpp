33bd73bed45f71283e3eb6f0d7afc141b6e20333
Anish Nilajkar <anishsnilajkar@gmail.com>
2024-08-19 21:38:54 +0530

    Add files via upload

---
+++ Pair.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    pair<int,int>p={1,3};
+    //Pair is used to store couple of values
+    cout<<p.first<<" "<<p.second;
+    return 0;
+}
---
+++ vector.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<pair<int,int>> v;
+    v.push_back({1,8});
+    cout<<v[0];
+    return 0;
+}
