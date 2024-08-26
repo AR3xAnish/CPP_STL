3b24c5ce5449ec12635cfc6b08a4148a7a62959b
Anish Nilajkar <anishsnilajkar@gmail.com>
2024-08-26 12:32:57 +0530

    Add files via upload

---
+++ permutation.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s="123";
+    // do{
+    //     cout<<s<<endl;
+    // }while(next_permutation(s.begin,s.end()));
+    cout<<next_permutation(s.begin,s.end());
+}
